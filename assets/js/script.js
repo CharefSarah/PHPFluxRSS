18537a470746601fab4e8500356bd9e67494aeaa
theolucas76 <76164072+theolucas76@users.noreply.github.com>
2021-02-10 10:41:00 +0100

    theme netflux terminé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+    document.getElementById("btnOpen").style.display = "none";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("btnOpen").style.display = "block";
   }
+
+ 
